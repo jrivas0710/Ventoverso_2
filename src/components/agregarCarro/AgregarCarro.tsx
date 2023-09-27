cb1f0525602bc45961f96b805e114c974c72cf35
berioska.monroy <berioska.monroy@gmail.com>
2023-09-27 02:58:41 -0300

    agregue desplegable al agregar carrito

--- src/components/agregarCarro/AgregarCarro.tsx
+++ src/components/agregarCarro/AgregarCarro.tsx
@@ -1,8 +1,17 @@
+import { useState } from 'react'
 import './AgregarCarro.css'
 
 
 
 export const AgregarCarro = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const estadoDiv = isOpen ? true: false
+
+    const openClose = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <>
 
@@ -21,10 +30,21 @@ export const AgregarCarro = () => {
 
                 <div>
                     <div className='cantidad'>
-                        <button className='botonCantidad'>Cantidad:&nbsp;<b> 1 unidad</b>&nbsp;&nbsp; <div className='flecha'> <span className="material-symbols-outlined">
-                        keyboard_control_key
-                    </span></div>
-                       </button>
+                        <button onClick={openClose} className='botonCantidad'>Cantidad:&nbsp;<b> 1 unidad</b>&nbsp;&nbsp; <div className='flecha'> <span className="material-symbols-outlined">
+                            keyboard_control_key
+                        </span></div>
+                        </button>
+                        
+            <div className='desplegable' hidden = {estadoDiv}>
+                <ul>
+                    <li>2 unidades</li>
+                    <li>3 unidades</li>
+                    <li>4 unidades</li>
+                    <li>5 unidades</li>
+                    <li>6 unidades</li>
+                </ul>
+            </div>
+
                     </div>
                     <div className='botonComprar-like'>
                         <div><button className='botonComprar'>Comprar</button></div>
@@ -36,17 +56,18 @@ export const AgregarCarro = () => {
                 <div className='compararCompartir'>
                     <div >
                         <span>
-                            <svg className='compare'fill="#000000" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><defs></defs><title>compare</title><path d="M28,6H18V4a2,2,0,0,0-2-2H4A2,2,0,0,0,2,4V24a2,2,0,0,0,2,2H14v2a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V8A2,2,0,0,0,28,6ZM4,15h6.17L7.59,17.59,9,19l5-5L9,9,7.59,10.41,10.17,13H4V4H16V24H4ZM16,28V26a2,2,0,0,0,2-2V8H28v9H21.83l2.58-2.59L23,13l-5,5,5,5,1.41-1.41L21.83,19H28v9Z" transform="translate(0)"> </path></g></svg>&nbsp;&nbsp;
+                            <svg className='compare' fill="#000000" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><defs></defs><title>compare</title><path d="M28,6H18V4a2,2,0,0,0-2-2H4A2,2,0,0,0,2,4V24a2,2,0,0,0,2,2H14v2a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V8A2,2,0,0,0,28,6ZM4,15h6.17L7.59,17.59,9,19l5-5L9,9,7.59,10.41,10.17,13H4V4H16V24H4ZM16,28V26a2,2,0,0,0,2-2V8H28v9H21.83l2.58-2.59L23,13l-5,5,5,5,1.41-1.41L21.83,19H28v9Z" transform="translate(0)"> </path></g></svg>&nbsp;&nbsp;
                         </span>
                         <span>Comparar</span>
                     </div>
-                    <div>
-                    </div><span className="material-symbols-outlined compartirImagen" >
+
+                    <div className='compartirImagen'><span className="material-symbols-outlined " >
                         share
-                    </span>
+                    </span></div>
                     <span className='compartirTexto'>Compartir</span>
                 </div>
             </div>
+
 
 
             <div className='devolGarantPuntos'>
