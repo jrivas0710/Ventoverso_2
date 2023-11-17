ef41f55c585d4969feea691a8900b0492807f55f
berioska.monroy <berioska.monroy@gmail.com>
2023-11-17 20:09:20 -0300

    lisa api productos similares

--- src/components/productosSimilares/ProductosSimilares.tsx
+++ src/components/productosSimilares/ProductosSimilares.tsx
@@ -1,9 +1,35 @@
 import './ProductosSimilares.css'
 import { Destacado } from '../../interfaces/Destacados'
+import { useEffect, useState } from 'react';
 
 
 
-export const ProductosSimilares = (props: { dataProductosSimilares: Destacado[] }) => {
+export const ProductosSimilares = () => {
+
+    
+    const [similares, setSimilares] = useState<Destacado[]>();
+
+
+    useEffect(() => {
+
+        fetch(` http://localhost:3000/categoria`, { // de aqui se traen y se muestran en el home, quien define cuantos se muestran en el home ?
+            method: "GET"
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log(response)
+                    return response.json() as Promise<Destacado[]>
+                }
+            })
+            .then(data => {
+                
+                setSimilares(data);
+            })
+            .catch(error => console.log(error))
+    }, [])
+
+
+
 
 
     return (
@@ -16,13 +42,13 @@ export const ProductosSimilares = (props: { dataProductosSimilares: Destacado[] 
 
 
 
-                    {props.dataProductosSimilares.map(item => {
+                    { similares && similares.map(item => {
                         return (
 
                             <div key={item.id}>
                                 <div className='producto'><img src={item.imagenUrl} alt={item.nombre} /></div>
                                 <div className='nombre-precio-ranking'>
-                                    {item.estrellas}
+                                  <span hidden>{item.estrellas}</span>  
                                     <a href='#'><img src="images/estrellas.png" alt="ranking" className='ranking' /></a>
                                     <span className='nombre-producto'>{item.nombre}</span>
                                     <span className='precio'>{item.precio}</span>
